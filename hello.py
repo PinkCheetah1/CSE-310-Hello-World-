125693d45cd70ef30d47b97d99a528277f6f98eb
PinkCheetah1 <66535303+PinkCheetah1@users.noreply.github.com>
2025-01-13 14:22:04 -0700

    Finished simple hello world program

---
+++ hello.py
@@ -0,0 +1,12 @@
+"""
+Simple "Hello World" program that prints the words "Hello World!" 
+Portfolio piece
+
+"""
+
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
